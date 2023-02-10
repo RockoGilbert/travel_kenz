d76a3706ab9dc11496175ac150f8afc6e165266c
RockoGilbert <evolve2122@gmail.com>
2023-02-09 19:16:53 -0600

    Made the carousel disappear on smaller screens and tweaked the responsiveness of each section. The front end is almost complete!

--- src/components/SelectsImg/SelectsImg.js
+++ src/components/SelectsImg/SelectsImg.js
@@ -1,20 +1,3 @@
-// import React from 'react'
-// import "./SelectsImgStyles.css"
-
-
-// function SelectsImg(props) {
-//     return (
-//         <div className="selects-location ">
-//             <img src={props.bgImg} alt="/" />
-//             <div className="overlay">
-//                 <p>{props.text}</p>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default SelectsImg
-
 import React from 'react'
 import './SelectsImgStyles.css'
 
